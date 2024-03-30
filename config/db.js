0d7773ec2684442d1362defadae4a28b26033ec5
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-03-30 22:46:25 +0300

    sequelize code upload

--- config/db.js
+++ config/db.js
@@ -1,1 +1,7 @@
 const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('diarybook', 'postgres', '1234567', {
+	host: 'localhost',
+	dialect: 'postgres',
+});
+module.exports = sequelize;
